b869d17628821fae336b6d1356c799e97ab69450
Zack <Zacharyjohnson@hotmail.com>
2024-10-09 23:10:46 -0500

    asdf

--- lambda/writeToS3/writeToS3/writeToS3.mjs
+++ lambda/writeToS3/writeToS3/writeToS3.mjs
@@ -14,28 +14,42 @@ import AWS from '@aws-sdk/client-s3';
 const S3 = new AWS.S3();
 
 export const writeToS3Handler = async (event, context) => {
-  const fileToUpload = {
-    userId:"123456",
-    email:"enrico@gmail.com",
-    city:"London",
-    country:"UK"
-  }
+  try
+  {
+    const fileToUpload = {
+      userId:"123456",
+      email:"enrico@gmail.com",
+      city:"London",
+      country:"UK"
+    }
 
-  const params = {
-    Bucket: 'writetos3',
-    Key: `test.json`,
-    Body: JSON.stringify(fileToUpload),
-    ContentType: 'application/json; charset=utf-8'
-  }
-  await S3.putObject(params).promise();
+    const params = {
+      Bucket: 'writetos3',
+      Key: `test.json`,
+      Body: JSON.stringify(fileToUpload),
+      ContentType: 'application/json; charset=utf-8'
+    }
+    await S3.putObject(params);
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'hello world',
-    })
-  };
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'hello world',
+      })
+    };
 
   return response;
+  }
+  catch (error)
+  {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: error,
+      })
+    };
+
+    return response;
+  }
 };
   
